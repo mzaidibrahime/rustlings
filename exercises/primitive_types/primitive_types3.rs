2f70e74b5bccffd1559d3f3b5552a90d80ef5194
mzaidibrahime <mzaidibrahime@gmail.com>
2023-08-02 08:56:32 +0100

    if, quiz1 and primitive_types

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,14 +5,19 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+
+    //array of size 100, initialises all the elements to value 20
+    let a:[u32;100] = [20; 100];
+    // array of size 2, every element is initialised separately
+    let b = [1,2];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
     }
+    println!("{}, {} size:{}", a[0], a[1], a.len());
+    println!("{}, {} size:{}", b[0], b[1], b.len());
 }
